6ce902eb14b5633a6faa4c16335a4e9b37c6612e
wayne1tam <waynevick7226@hotmail.com>
2021-03-23 22:02:07 -0400

    Format file

--- california-house-market/housing.py
+++ california-house-market/housing.py
@@ -1,10 +1,11 @@
 import os
 import tarfile
-import urllib
+import urllib.request
 import pandas as pd
 import matplotlib.pyplot as plt
 from zlib import crc32
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
@@ -22,23 +23,13 @@ def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
 
-fetch_housing_data()    
+def test_set_check(identifier, test_ratio):
+    return crc32(np.int64(identifier)) & 0xffffff < test_ratio * 2**32
 
-housing = load_housing_data()
-housing.head()
-
-housing.info()
-
-housing['ocean_proximity'].value_counts()
-
-housing.describe()
-
-
-housing.hist(bins=50, figsize=(20,15))
-plt.show()
-
-
-
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids=data[id_column]
+    in_test_set=ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set] 
 
 def split_train_test(data, test_ratio):
     shuffled_indices = np.random.permutation(len(data))
@@ -47,27 +38,28 @@ def split_train_test(data, test_ratio):
     train_indices = shuffled_indices[test_set_size:]
     return data.iloc[train_indices], data.iloc[test_indices]
 
+fetch_housing_data()    
+
+housing = load_housing_data()
+print(housing.head())
+print(housing.info())
+
+housing['ocean_proximity'].value_counts()
+
+housing.describe()
+
+housing.hist(bins=50, figsize=(20,15))
+plt.show()
+
 train_set, test_set = split_train_test(housing, 0.2)    
 
-len(train_set)
-
-len(test_set)
-
-
-def test_set_check(identifier, test_ratio):
-    return crc32(np.int64(identifier)) & 0xffffff < test_ratio * 2**32
-
-def split_train_test_by_id(data, test_ratio, id_column):
-    ids=data[id_column]
-    in_test_set=ids.apply(lambda id_: test_set_check(id_, test_ratio))
-    return data.loc[~in_test_set], data.loc[in_test_set]    
-
+print(len(train_set))
+print(len(test_set))
+   
 housing_with_id=housing.reset_index() #adds an index column
 train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")    
 
 housing_with_id["id"] = housing["longitude"] * 1000 + housing["latitude"]
 train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "id")
-
-from sklearn.model_selection import train_test_split
 
 train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
