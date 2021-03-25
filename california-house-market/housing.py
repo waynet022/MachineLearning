1293723bd3aa0c085dbe455cf8b9414ffb20d5e0
wayne1tam <waynevick7226@hotmail.com>
2021-03-25 02:38:05 -0400

    Display previous outputs

--- california-house-market/housing.py
+++ california-house-market/housing.py
@@ -12,14 +12,20 @@ HOUSING_PATH = os.path.join("datasets", "housing")
 HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
 
 def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    #creates data/housing directory
     os.makedirs(housing_path, exist_ok=True)
     tgz_path = os.path.join(housing_path, "housing.tgz")
+    
+    #downloads the housing file
     urllib.request.urlretrieve(housing_url, tgz_path)
+    
+    #extracts the housing.csv file into directory
     housing_tgz = tarfile.open(tgz_path)
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
 
 def load_housing_data(housing_path=HOUSING_PATH):
+    # loads data into dataframe and returns it
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
 
@@ -39,14 +45,12 @@ def split_train_test(data, test_ratio):
     return data.iloc[train_indices], data.iloc[test_indices]
 
 fetch_housing_data()    
-
 housing = load_housing_data()
+
 print(housing.head())
 print(housing.info())
-
-housing['ocean_proximity'].value_counts()
-
-housing.describe()
+print(housing['ocean_proximity'].value_counts())
+print(housing.describe())
 
 housing.hist(bins=50, figsize=(20,15))
 plt.show()
@@ -63,3 +67,6 @@ housing_with_id["id"] = housing["longitude"] * 1000 + housing["latitude"]
 train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "id")
 
 train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+
+housing['income_cat'] = pd.cut(housing['median_income'], bins=[0.,1.5, 3.0, 4.5, 6., np.inf], labels=[1,2,3,4,5])
+housing['income_cat'].hist()
